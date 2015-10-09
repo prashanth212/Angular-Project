b88c670aa85ad85e668e3b155e5d0ae8b3028424
prashanth212 <prashanth212reddy@gmail.com>
2015-10-09 11:27:24 -0500

    register page

--- src/app.js
+++ src/app.js
@@ -11,32 +11,50 @@ angular.module('yashCRM', ['ui.router', 'ngCookies'])
             })
 
         .state('login', {
-            url: "/login",
-            templateUrl: "login/login.tmpl.html"
-        })
+                url: "/login",
+                templateUrl: "login/login.tmpl.html",
+                controller: 'LoginCtrl'
+            })
+            .state('register', {
+                url: "/register",
+                templateUrl: "register/register.tmpl.html",
+                requireLogin: true
+            })
+            .state('myaccounts', {
+                url: "/myaccounts",
+                templateUrl: "myaccount/myaccount.tmpl.html",
+                requireLogin: true
+            })
+
         .state('welcome', {
             url: "/welcome",
-            templateUrl: "home/home.tmpl.html"
+            templateUrl: "home/home.tmpl.html",
+            requireLogin: false
         })
 
-        .state('myaccounts', {
-            url: "/myaccounts",
-            templateUrl: "myaccount/myaccount.tmpl.html"
-        })
+
 
     })
-.run(['$rootScope', '$location', '$cookieStore', '$http',
+    .run(['$rootScope', '$location', '$cookieStore', '$http',
     function ($rootScope, $location, $cookieStore, $http) {
-        // keep user logged in after page refresh
-        $rootScope.globals = $cookieStore.get('globals') || {};
-        if ($rootScope.globals.currentUser) {
-            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
-        }
-  
-        $rootScope.$on('$locationChangeStart', function (event, next, current) {
-            // redirect to login page if not logged in
-            if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
-                $location.path('/main');
+            // keep user logged in after page refresh
+            $rootScope.globals = $cookieStore.get('globals') || {};
+            if ($rootScope.globals.currentUser) {
+                $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
             }
-        });
+
+            $rootScope.$on('$locationChangeStart', function (event, next, current) {
+                // redirect to login page if not logged in
+                if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
+                    $location.path('/main');
+                }
+            });
+            $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
+                var requireLogin = toState.data.requireLogin;
+
+                if (requireLogin && typeof $rootScope.currentUser === 'undefined') {
+                    event.preventDefault();
+                    // get me a login modal!
+                }
+            });
     }]);
